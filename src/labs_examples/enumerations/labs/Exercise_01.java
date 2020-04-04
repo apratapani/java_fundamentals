7466297e60fb973cc41d59d68840df35168f23ec
Anil Pratapani <apratapani@gmail.com>
2020-04-04 07:22:26 -0500

    Enum Excercises

--- src/labs_examples/enumerations/labs/Exercise_01.java
+++ src/labs_examples/enumerations/labs/Exercise_01.java
@@ -8,3 +8,73 @@ package labs_examples.enumerations.labs;
  *      of this enum from a seperate class.
  */
 
+
+enum NaftaCountries {
+
+    USA,MEXICO,CANADA;
+
+}
+
+
+enum  Oceans {
+
+    // Avg depth is in meters, volume is in Km3
+
+    ARCTIC(1205,18750000),SOUTHERN(3270,71800000),INDIAN(3741,264000000),ATLANTIC(3646,310410900),PACIFIC(3970,669880000);
+
+     int avgDepth;
+     double vol;
+
+
+     final String DUOM = "meters";
+     final String VUOM ="km3";
+
+
+    Oceans(int avgDepth,double vol) {
+
+        this.avgDepth = avgDepth;
+        this.vol = vol;
+
+    }
+
+    public int getAvgDepth() {
+        return avgDepth;
+    }
+
+    public void setAvgDepth(int avgDepth) {
+        this.avgDepth = avgDepth;
+    }
+
+    public double getVol() {
+        return vol;
+    }
+
+    public void setVol(double vol) {
+        this.vol = vol;
+    }
+}
+
+
+
+class DemoEnum {
+
+
+    public static void main(String[] args) {
+
+        NaftaCountries myCountry = NaftaCountries.USA;
+
+        System.out.println("I like  " + myCountry);;
+
+
+        for(Oceans val : Oceans.values()) {
+
+            System.out.print(val + " ocean has an average depth of " + val.getAvgDepth() + " " + val.DUOM);
+            System.out.println(" it has volume of " + val.getVol() + " " + val.VUOM);
+
+        }
+
+
+    }
+
+
+}
